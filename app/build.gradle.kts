de4b9cc9623a691ea1640a0c696f535b4cfdf35e
wonna-0830 <pjwon0830@naver.com>
2025-06-16 17:33:53 +0900

    메인 화면 FAB 메뉴 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,9 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +51,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- app/src/main/java/com/example/timecapsule/MainActivity.kt
+++ app/src/main/java/com/example/timecapsule/MainActivity.kt
@@ -1,9 +1,13 @@
 package com.example.timecapsule
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -13,22 +17,22 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.timecapsule.ui.theme.TimeCapsuleTheme
 
-class MainActivity : ComponentActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TimeCapsuleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+
+        // 스플래시 전용 레이아웃 사용 (필요 시 너가 만든 배경화면 등 넣기)
+        setContentView(R.layout.activity_main)
+
+        // 5초(5000ms) 후에 MainDiary로 이동
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainDiary::class.java)
+            startActivity(intent)
+            finish() // MainActivity는 종료
+        }, 5000)
     }
 }
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
